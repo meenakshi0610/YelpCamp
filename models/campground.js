36905aed336b47cf20ec61e286f710b226309cd6
Shubham Jain <shubhamj99.sj@gmail.com>
2021-02-17 19:01:47 +0530

    All Features Added

---
+++ models/campground.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const { campgroundSchema } = require('../Schemas');
+const reviews = require('./reviews');
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    images: [
+        {    
+            url: String,
+            filename: String
+        }
+    ],
+    price: Number,
+    description: String,
+    location: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+CampgroundSchema.post('findOneAndDelete', async function (doc){
+    if(doc){
+        await reviews.remove({
+            _id:{
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
