1c592e08dceb9094167f1b8cc9c2ce1a175e89c8
Shubham Jain <shubhamj99.sj@gmail.com>
2021-02-24 23:37:15 +0530

    Production Ready

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const helmet = require('helmet');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
+const MongoStore = require('connect-mongo').default;
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 const port = process.env.PORT || 3000;
@@ -49,8 +50,17 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    secret,
+    touchAfter: 24*3600
+});
 
+store.on("error",function (e){
+    console.log("SESSION STORE ERROR", e);
+});
 const sessionConfig = {
+    store,
     name: '14b2cd8k9lgdt5n',
     secret,
     resave: false,
